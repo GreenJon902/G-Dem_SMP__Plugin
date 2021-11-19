986b78f49bef3050891e83a96a8147254a902ce7
GreenJon902 <GreenJon902@gmail.com>
2021-11-19 15:33:04 +0000

    added Comonent loading and PluginComponent interface

--- src/main/java/com/greenjon902/g_dem__smp/G_Dem__SMP.java
+++ src/main/java/com/greenjon902/g_dem__smp/G_Dem__SMP.java
@@ -5,10 +5,31 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.logging.Logger;
 
 public final class G_Dem__SMP extends JavaPlugin {
+    private static final PluginComponent[] components =
+            {
+    };
+
+
     @Override
     public void onEnable() {
         Logger logger = getLogger();
+
         logger.info("Starting the G-Dem SMP plugin...");
+
+        int component_index;
+        PluginComponent component;
+
+        for (component_index=0; component_index < components.length; component_index++) {
+            component = components[component_index];
+            logger.info("Setting up " + component.getClass().toString());
+            component.setup(this);
+        }
+
+        for (component_index=0; component_index < components.length; component_index++) {
+            component = components[component_index];
+            logger.info("Enabling " + component.getClass().toString());
+            component.enable(this);
+        }
 
         logger.info("Started the G-Dem SMP plugin...");
 
@@ -17,7 +38,15 @@ public final class G_Dem__SMP extends JavaPlugin {
     @Override
     public void onDisable() {
         Logger logger = getLogger();
+
         logger.info("Ending the G-Dem SMP plugin...");
+
+        int component_index;
+        PluginComponent component;
+        for (component_index=0; component_index < components.length; component_index++) {
+            component = components[component_index];
+            component.end(this);
+        }
 
         logger.info("Ended the G-Dem SMP plugin...");
     }
---
+++ src/main/java/com/greenjon902/g_dem__smp/PluginComponent.java
@@ -0,0 +1,10 @@
+package com.greenjon902.g_dem__smp;
+
+public interface PluginComponent {
+    public void setup(G_Dem__SMP mainClass);
+
+    public void enable(G_Dem__SMP mainClass);
+
+    public void end(G_Dem__SMP mainClass);
+
+}
