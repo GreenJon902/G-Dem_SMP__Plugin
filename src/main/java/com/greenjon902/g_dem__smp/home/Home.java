ff51537e58234a40102ce31132b076f710c96b81
GreenJon902 <GreenJon902@gmail.com>
2021-11-19 15:38:24 +0000

    added plugin main class and added the component to the PluginComponent loader list

---
+++ src/main/java/com/greenjon902/g_dem__smp/home/Home.java
@@ -0,0 +1,21 @@
+package com.greenjon902.g_dem__smp.home;
+
+import com.greenjon902.g_dem__smp.G_Dem__SMP;
+import com.greenjon902.g_dem__smp.PluginComponent;
+
+public class Home implements PluginComponent {
+    @Override
+    public void setup(G_Dem__SMP mainClass) {
+
+    }
+
+    @Override
+    public void enable(G_Dem__SMP mainClass) {
+
+    }
+
+    @Override
+    public void end(G_Dem__SMP mainClass) {
+
+    }
+}
