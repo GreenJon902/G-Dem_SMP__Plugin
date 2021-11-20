4e85ac5d5133a10f21d9f9ec0c5b71878955d172
GreenJon902 <GreenJon902@gmail.com>
2021-11-20 19:51:00 +0000

    added commands

--- src/main/java/com/greenjon902/g_dem__smp/home/Home.java
+++ src/main/java/com/greenjon902/g_dem__smp/home/Home.java
@@ -2,11 +2,20 @@ package com.greenjon902.g_dem__smp.home;
 
 import com.greenjon902.g_dem__smp.G_Dem__SMP;
 import com.greenjon902.g_dem__smp.PluginComponent;
+import com.greenjon902.g_dem__smp.home.commands.CommandDelHome;
+import com.greenjon902.g_dem__smp.home.commands.CommandHome;
+import com.greenjon902.g_dem__smp.home.commands.CommandSetHome;
+import com.greenjon902.g_dem__smp.sit.commands.CommandSit;
 
 public class Home implements PluginComponent {
     @Override
     public void setup(G_Dem__SMP mainClass) {
-
+        //noinspection ConstantConditions
+        mainClass.getCommand("setHome").setExecutor(new CommandSetHome());
+        //noinspection ConstantConditions
+        mainClass.getCommand("home").setExecutor(new CommandHome());
+        //noinspection ConstantConditions
+        mainClass.getCommand("delHome").setExecutor(new CommandDelHome());
     }
 
     @Override
