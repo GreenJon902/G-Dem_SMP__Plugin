5a295d896843e2fb43a5e87804def1e8720392b5
GreenJon902 <GreenJon902@gmail.com>
2021-11-21 17:53:21 +0000

    added file loading and config

--- src/main/java/com/greenjon902/g_dem__smp/homes/HomesComponentStorage.java
+++ src/main/java/com/greenjon902/g_dem__smp/homes/HomesComponentStorage.java
@@ -1,9 +1,65 @@
 package com.greenjon902.g_dem__smp.homes;
 
 import com.greenjon902.g_dem__smp.G_Dem__SMP;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.UUID;
 
 public class HomesComponentStorage {
+    private File configFile;
+    private FileConfiguration config;
+    private File homesFolder;
+    private HashMap<UUID, FileConfiguration> homes;
+
+    private void checkFiles() {
+        configFile = new File(G_Dem__SMP.getInstance().getDataFolder(), "homes/config.yml");
+        if (!configFile.exists()) {
+            //noinspection ResultOfMethodCallIgnored
+            configFile.getParentFile().mkdirs();
+            G_Dem__SMP.getInstance().saveResource("homes/config.yml", false);
+        }
+
+        homesFolder = new File(G_Dem__SMP.getInstance().getDataFolder(), "homes/playerHomes");
+        if (!homesFolder.exists()) {
+            //noinspection ResultOfMethodCallIgnored
+            homesFolder.mkdirs();
+        }
+    }
+
     public void load() {
+        checkFiles();
+
+        config = new YamlConfiguration();
+        try {
+            config.load(configFile);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+
+
+        homes = new HashMap<>();
+
+        File[] files = homesFolder.listFiles();
+        int file_index;
+        File file;
+        //noinspection ConstantConditions
+        for (file_index = 0; file_index < files.length; file_index++) {
+            file = files[file_index];
+            config = new YamlConfiguration();
+
+            try {
+                config.load(file);
+            } catch (IOException | InvalidConfigurationException e) {
+                e.printStackTrace();
+            }
+
+            homes.put(UUID.fromString(file.getName().replace(".yml", "")), config);
+        }
     }
 
     public void save() {
