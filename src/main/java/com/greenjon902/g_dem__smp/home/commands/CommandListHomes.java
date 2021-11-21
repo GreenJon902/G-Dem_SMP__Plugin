29e217c43f997bbc72d3fe6349e7b16461517c61
GreenJon902 <GreenJon902@gmail.com>
2021-11-21 09:13:54 +0000

    added listHomes and listAllHomes commands and permissions

---
+++ src/main/java/com/greenjon902/g_dem__smp/home/commands/CommandListHomes.java
@@ -0,0 +1,12 @@
+package com.greenjon902.g_dem__smp.home.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CommandListHomes implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
+}
