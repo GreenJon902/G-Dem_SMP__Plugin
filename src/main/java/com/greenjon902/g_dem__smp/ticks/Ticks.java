220ae3b703cfaafc5451213c442d4985a1d41bdb
GreenJon902 <37347905+GreenJon902@users.noreply.github.com>
2021-11-23 07:31:10 +0000

    Create Ticks.java

---
+++ src/main/java/com/greenjon902/g_dem__smp/ticks/Ticks.java
@@ -0,0 +1,23 @@
+package com.greenjon902.g_dem__smp.ticks;
+
+import com.greenjon902.g_dem__smp.G_Dem__SMP;
+import com.greenjon902.g_dem__smp.PluginComponent;
+
+
+public class Ticks implements PluginComponent {
+
+    @Override
+    public void setup(G_Dem__SMP mainClass) {
+
+    }
+
+    @Override
+    public void enable(G_Dem__SMP mainClass) {
+
+    }
+
+    @Override
+    public void end(G_Dem__SMP mainClass) {
+
+    }
+}
