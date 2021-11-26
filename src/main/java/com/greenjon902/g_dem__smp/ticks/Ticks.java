599b681e46022f60a56dcc2d2d59184e476cb778
GreenJon902 <GreenJon902@gmail.com>
2021-11-26 19:30:09 +0000

    added interval for tick records

--- src/main/java/com/greenjon902/g_dem__smp/ticks/Ticks.java
+++ src/main/java/com/greenjon902/g_dem__smp/ticks/Ticks.java
@@ -2,6 +2,7 @@ package com.greenjon902.g_dem__smp.ticks;
 
 import com.greenjon902.g_dem__smp.G_Dem__SMP;
 import com.greenjon902.g_dem__smp.PluginComponent;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
 
@@ -32,11 +33,16 @@ public class Ticks implements PluginComponent {
 
     @Override
     public void enable(G_Dem__SMP mainClass) {
-
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(mainClass, Ticks::takeRecord, 0,
+                20L *60*config.getInt("record-interval"));
     }
 
     @Override
     public void end(G_Dem__SMP mainClass) {
 
+    }
+
+    public static void takeRecord() {
+        System.out.println("Make record function ran but it ain got nothin in it");
     }
 }
