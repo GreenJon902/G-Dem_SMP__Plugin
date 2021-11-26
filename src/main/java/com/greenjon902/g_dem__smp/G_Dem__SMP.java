2f3139ed0e0cea966d51f154f2cecee42cd1ac30
GreenJon902 <GreenJon902@gmail.com>
2021-11-26 20:55:08 +0000

    removed sit from ticks

--- src/main/java/com/greenjon902/g_dem__smp/G_Dem__SMP.java
+++ src/main/java/com/greenjon902/g_dem__smp/G_Dem__SMP.java
@@ -1,7 +1,6 @@
 package com.greenjon902.g_dem__smp;
 
 
-import com.greenjon902.g_dem__smp.sit.Sit;
 import com.greenjon902.g_dem__smp.ticks.Ticks;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,7 +11,6 @@ public final class G_Dem__SMP extends JavaPlugin {
     private static final PluginComponent[] components =
             {
                     new Ticks(),
-                    new Sit(),
     };
   
     private static G_Dem__SMP instance;
