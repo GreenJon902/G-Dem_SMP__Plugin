f8edf2cc418c39a3aecb6a2b742b1107be2dff16
GreenJon902 <GreenJon902@gmail.com>
2021-11-26 20:28:42 +0000

    added recording when player joins or leaves the game

--- src/main/java/com/greenjon902/g_dem__smp/ticks/Ticks.java
+++ src/main/java/com/greenjon902/g_dem__smp/ticks/Ticks.java
@@ -2,28 +2,29 @@ package com.greenjon902.g_dem__smp.ticks;
 
 import com.greenjon902.g_dem__smp.G_Dem__SMP;
 import com.greenjon902.g_dem__smp.PluginComponent;
-import org.bukkit.Bukkit;
-import org.bukkit.configuration.InvalidConfigurationException;
-import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 
 public class Ticks implements PluginComponent {
-    File ticksFile;
+    private static File ticksFolder;
 
     @Override
     public void setup(G_Dem__SMP mainClass) {
-        ticksFile = new File(G_Dem__SMP.getInstance().getDataFolder(), "/ticks/records/");
-        if (!ticksFile.exists()) {
+        ticksFolder = new File(G_Dem__SMP.getInstance().getDataFolder(), "/ticks/records");
+        if (!ticksFolder.exists()) {
             //noinspection ResultOfMethodCallIgnored
-            ticksFile.mkdirs();
+            ticksFolder.mkdirs();
         }
     }
 
     @Override
     public void enable(G_Dem__SMP mainClass) {
+        mainClass.getServer().getPluginManager().registerEvents(new PlayerSessionEvents(), mainClass);
     }
 
     @Override
@@ -31,7 +32,15 @@ public class Ticks implements PluginComponent {
 
     }
 
-    public static void takeRecord() {
-        System.out.println("Make record function ran but it ain got nothin in it");
+    public static void takeRecord(Player player) {
+        try {
+            BufferedWriter output = new BufferedWriter(new FileWriter(new File(ticksFolder, String.valueOf(player.getUniqueId()) + ".txt"), true));
+            output.write(",");
+            output.write(String.valueOf(player.getTicksLived()));
+            output.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
