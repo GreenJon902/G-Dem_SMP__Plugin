4e2675e9c14c397917d03c09a74d01566b66b4e1
GreenJon902 <GreenJon902@gmail.com>
2021-11-26 19:21:10 +0000

    added config file

--- src/main/java/com/greenjon902/g_dem__smp/ticks/Ticks.java
+++ src/main/java/com/greenjon902/g_dem__smp/ticks/Ticks.java
@@ -3,12 +3,20 @@ package com.greenjon902.g_dem__smp.ticks;
 import com.greenjon902.g_dem__smp.G_Dem__SMP;
 import com.greenjon902.g_dem__smp.PluginComponent;
 
+import java.io.File;
+
 
 public class Ticks implements PluginComponent {
+    File configFile;
 
     @Override
     public void setup(G_Dem__SMP mainClass) {
-
+        configFile = new File(G_Dem__SMP.getInstance().getDataFolder(), "/ticks/config.yml");
+        if (!configFile.exists()) {
+            //noinspection ResultOfMethodCallIgnored
+            configFile.getParentFile().mkdirs();
+            G_Dem__SMP.getInstance().saveResource("ticks/config.yml", false);
+        }
     }
 
     @Override
