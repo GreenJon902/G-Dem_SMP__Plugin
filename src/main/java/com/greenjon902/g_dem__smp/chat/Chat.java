5be4b5382eaaa307354e4f546d1532db08c5575c
GreenJon902 <GreenJon902@gmail.com>
2021-12-02 19:46:52 +0000

    now extends PluginComponent

--- src/main/java/com/greenjon902/g_dem__smp/chat/Chat.java
+++ src/main/java/com/greenjon902/g_dem__smp/chat/Chat.java
@@ -1,22 +1,21 @@
 package com.greenjon902.g_dem__smp.chat;
 
-import com.greenjon902.g_dem__smp.G_Dem__SMP;
 import com.greenjon902.g_dem__smp.PluginComponent;
 
-public class Chat implements PluginComponent {
+public class Chat extends PluginComponent {
 
     @Override
-    public void setup(G_Dem__SMP mainClass) {
+    public void setup() {
 
     }
 
     @Override
-    public void enable(G_Dem__SMP mainClass) {
+    public void enable() {
 
     }
 
     @Override
-    public void end(G_Dem__SMP mainClass) {
+    public void end() {
 
     }
 }
