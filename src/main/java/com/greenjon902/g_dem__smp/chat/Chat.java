fe45466e8fdccfa6e26a4ceb16050abe0c20a847
GreenJon902 <GreenJon902@gmail.com>
2021-12-04 12:28:10 +0000

    added chat message sending

--- src/main/java/com/greenjon902/g_dem__smp/chat/Chat.java
+++ src/main/java/com/greenjon902/g_dem__smp/chat/Chat.java
@@ -2,6 +2,8 @@ package com.greenjon902.g_dem__smp.chat;
 
 import com.greenjon902.g_dem__smp.G_Dem__SMP;
 import com.greenjon902.g_dem__smp.PluginComponent;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 public class Chat implements PluginComponent {
 
@@ -18,5 +20,17 @@ public class Chat implements PluginComponent {
     @Override
     public void end() {
 
+    }
+
+    public void sendMessage(String messageId, String componentId, Player player) {
+        // TODO: Load actual message and stuff
+        player.sendMessage("[" + componentId + "]  " + messageId);
+    }
+
+    public void announceMessage(String messageId, String componentId) {
+        // TODO: Load actual message and stuff
+        for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+            player.sendMessage("[" + componentId + "]  " + messageId);
+        }
     }
 }
--- src/main/java/com/greenjon902/g_dem__smp/chat/ChatAPI.java
+++ src/main/java/com/greenjon902/g_dem__smp/chat/ChatAPI.java
@@ -1,5 +1,14 @@
 package com.greenjon902.g_dem__smp.chat;
 
-public class ChatAPI {
+import com.greenjon902.g_dem__smp.G_Dem__SMP;
+import org.bukkit.entity.Player;
 
+public class ChatAPI {
+    public static void sendMessage(String messageId, String componentId, Player player) {
+        ((Chat) G_Dem__SMP.getComponent("Chat")).sendMessage(messageId, componentId, player);
+    }
+
+    public static void announceMessage(String messageId, String componentId) {
+        ((Chat) G_Dem__SMP.getComponent("Chat")).announceMessage(messageId, componentId);
+    }
 }
