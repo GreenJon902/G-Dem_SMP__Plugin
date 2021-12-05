6b094a24bc81efcb8ef140eaaa3de11afaf25ad6
GreenJon902 <GreenJon902@gmail.com>
2021-12-05 15:56:20 +0000

    sit messaging system now new + Sit.toggle now returns state and Sit.standAll now returns amount stood

--- src/main/java/com/greenjon902/g_dem__smp/sit/commands/CommandStandAll.java
+++ src/main/java/com/greenjon902/g_dem__smp/sit/commands/CommandStandAll.java
@@ -1,16 +1,22 @@
 package com.greenjon902.g_dem__smp.sit.commands;
 
 import com.greenjon902.g_dem__smp.G_Dem__SMP;
+import com.greenjon902.g_dem__smp.chat.ChatAPI;
 import com.greenjon902.g_dem__smp.sit.Sit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import java.util.HashMap;
+
 public class CommandStandAll implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        ((Sit) G_Dem__SMP.getComponent("Sit")).standAll();
+        int amount = ((Sit) G_Dem__SMP.getComponent("Sit")).standAll();
+        ChatAPI.sendMessage("sit.commands.standAll.allStood", new HashMap<String, String>() {{
+            put("amountStood", String.valueOf(amount));
+        }}, "Sit", sender);
 
         return true;
     }
