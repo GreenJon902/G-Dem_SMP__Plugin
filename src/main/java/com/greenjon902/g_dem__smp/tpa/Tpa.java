d5a4b44598f285563b70c0058157dd20e73f36e8
GreenJon902 <GreenJon902@gmail.com>
2021-12-12 19:32:09 +0000

    added Tpa class and added it to G_Dem__SMP.components

---
+++ src/main/java/com/greenjon902/g_dem__smp/tpa/Tpa.java
@@ -0,0 +1,20 @@
+package com.greenjon902.g_dem__smp.tpa;
+
+import com.greenjon902.g_dem__smp.PluginComponent;
+
+public class Tpa implements PluginComponent {
+    @Override
+    public void setup() {
+
+    }
+
+    @Override
+    public void enable() {
+
+    }
+
+    @Override
+    public void end() {
+
+    }
+}
