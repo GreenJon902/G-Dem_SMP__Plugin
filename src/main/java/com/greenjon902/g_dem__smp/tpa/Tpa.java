03f430e2f6fd66c33672afba238b067cacaa02c1
GreenJon902 <GreenJon902@gmail.com>
2021-12-12 19:52:34 +0000

    registered commands

--- src/main/java/com/greenjon902/g_dem__smp/tpa/Tpa.java
+++ src/main/java/com/greenjon902/g_dem__smp/tpa/Tpa.java
@@ -1,11 +1,24 @@
 package com.greenjon902.g_dem__smp.tpa;
 
+import com.greenjon902.g_dem__smp.G_Dem__SMP;
 import com.greenjon902.g_dem__smp.PluginComponent;
+import com.greenjon902.g_dem__smp.tpa.commands.*;
 
 public class Tpa implements PluginComponent {
     @Override
     public void setup() {
-
+        //noinspection ConstantConditions
+        G_Dem__SMP.getInstance().getCommand("tpa").setExecutor(new CommandTpa());
+        //noinspection ConstantConditions
+        G_Dem__SMP.getInstance().getCommand("tpaAccept").setExecutor(new CommandTpaAccept());
+        //noinspection ConstantConditions
+        G_Dem__SMP.getInstance().getCommand("tpaDeny").setExecutor(new CommandTpaDeny());
+        //noinspection ConstantConditions
+        G_Dem__SMP.getInstance().getCommand("tpaHere").setExecutor(new CommandTpaHere());
+        //noinspection ConstantConditions
+        G_Dem__SMP.getInstance().getCommand("tpaHereAll").setExecutor(new CommandTpaHereAll());
+        //noinspection ConstantConditions
+        G_Dem__SMP.getInstance().getCommand("tpaMute").setExecutor(new CommandTpaMute());
     }
 
     @Override
