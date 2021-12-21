02aa6ec866a4d3a0b5da01c3010ebde7e9933fde
GreenJon902 <GreenJon902@gmail.com>
2021-12-21 20:44:00 +0000

    tab complete for ban word, list vanned words and unban word

--- src/main/java/com/greenjon902/g_dem__smp/chat/commands/CommandBanWord.java
+++ src/main/java/com/greenjon902/g_dem__smp/chat/commands/CommandBanWord.java
@@ -4,14 +4,22 @@ import com.greenjon902.g_dem__smp.G_Dem__SMP;
 import com.greenjon902.g_dem__smp.chat.Chat;
 import com.greenjon902.g_dem__smp.chat.ChatAPI;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 
-public class CommandBanWord implements CommandExecutor {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommandBanWord implements TabExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         ChatAPI.sendMessage("Banning word \"" + String.join(" ", args) + "\"!", "Chat", sender);
         ((Chat) G_Dem__SMP.getComponent("Chat")).banWord(String.join(" ", args));
         return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        return new ArrayList<>();
     }
 }
--- src/main/java/com/greenjon902/g_dem__smp/chat/commands/CommandListBannedWords.java
+++ src/main/java/com/greenjon902/g_dem__smp/chat/commands/CommandListBannedWords.java
@@ -4,12 +4,14 @@ import com.greenjon902.g_dem__smp.G_Dem__SMP;
 import com.greenjon902.g_dem__smp.chat.Chat;
 import com.greenjon902.g_dem__smp.chat.ChatAPI;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
-public class CommandListBannedWords implements CommandExecutor {
+public class CommandListBannedWords implements TabExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (args.length != 0) {
@@ -21,5 +23,10 @@ public class CommandListBannedWords implements CommandExecutor {
                     }}, "Chat", sender);
         }
         return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        return new ArrayList<>();
     }
 }
