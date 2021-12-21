61849f80123a037ec913ea5d9437dcb7477ce8c3
GreenJon902 <GreenJon902@gmail.com>
2021-12-21 21:31:07 +0000

    added FilterWithFunctionFunction class and TabCompleterHelper.filterWithFunction

--- src/main/java/com/greenjon902/tabCompleterHelper/TabCompleterHelper.java
+++ src/main/java/com/greenjon902/tabCompleterHelper/TabCompleterHelper.java
@@ -7,11 +7,23 @@ import java.util.Locale;
 public class TabCompleterHelper {
     public static List<String> noSolutions = new ArrayList<String>();
 
-    public static List<String> filter(List<String> allSolutions, String typedSolution) {
+    public static List<String> filter(List<String> allSolutions, String partialSolution) {
         ArrayList<String> filteredSolutions = new ArrayList<>();
 
         for (String solution : allSolutions) {
-            if (solution.toLowerCase(Locale.ROOT).startsWith(typedSolution.toLowerCase(Locale.ROOT))) {
+            if (solution.toLowerCase(Locale.ROOT).startsWith(partialSolution.toLowerCase(Locale.ROOT))) {
+                filteredSolutions.add(solution);
+            }
+        }
+        return filteredSolutions;
+    }
+
+    public static List<String> filterWithFunction(Object[] allSolutionsBeforeFunction, FilterWithFunctionFunction function, String partialSolution) {
+        ArrayList<String> filteredSolutions = new ArrayList<>();
+
+        for (Object solutionBeforeFunction : allSolutionsBeforeFunction) {
+            String solution = function.convert(solutionBeforeFunction);
+            if (solution.toLowerCase(Locale.ROOT).startsWith(partialSolution.toLowerCase(Locale.ROOT))) {
                 filteredSolutions.add(solution);
             }
         }
